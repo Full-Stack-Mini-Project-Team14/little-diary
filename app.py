0f3eac8b7c128dbbd0b488a7401565bd8a250222
ryu820 <rhju820@naver.com>
2023-02-11 21:03:20 +0900

    지금까지 완료된 부분 연결 업데이트 ->db업데이트는 추후 예정

--- app.py
+++ app.py
@@ -9,23 +9,46 @@ app = Flask(__name__)
 # ca = certifi.where()
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://dajung:sparta@cluster0.nea13z3.mongodb.net/cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.5omi5fs.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
-    return render_template('main.html')
+    return render_template('index.html')
 
-@app.route('/login')
+@app.route("/diary/write", methods=["POST"])
+def write_post():
+    title_receive = request.form['title_give']
+    contents_receive = request.form['contents_give']
+    tag_receive = request.form['tag_give']
+    emo_receive = request.form['emo_give']
+
+    doc = {
+        'title':title_receive,
+        'contents':contents_receive,
+        'tag':tag_receive,
+        'emo': emo_receive
+    }
+    db.write.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료'})
+
+
+@app.route('/diary/login')
 def login():
     return render_template('login.html')
 
-@app.route('/join')
+@app.route('/diary')
+def write():
+    return render_template('write.html')
+
+@app.route('/diary/join')
 def join():
     return render_template('join.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5002, debug=True)
 
 
